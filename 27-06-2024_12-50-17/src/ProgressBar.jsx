3b9547ad0a023ad6d328b6463961f48326830013
Ankit Patil <123143098+16SULPHUR@users.noreply.github.com>
2024-06-27 12:50:26 +0530

    Add files in folder 27-06-2024_12-50-17

---
+++ 27-06-2024_12-50-17/src/ProgressBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProgressBar = ({ progress }) => {
+  return (
+    <div className="progress-bar" style={{display: progress?"block":"none"}}>
+      <div
+        className="progress-bar-fill"
+        style={{ width: `${progress}%` }}
+      ></div>  <div>{progress}</div>
+    </div>
+  );
+};
+
+export default ProgressBar;
