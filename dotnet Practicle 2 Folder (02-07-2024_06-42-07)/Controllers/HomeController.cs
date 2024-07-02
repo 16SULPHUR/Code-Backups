323dce4cd9c1dcee66dce144f34808cc7672369d
Ankit Patil <123143098+16SULPHUR@users.noreply.github.com>
2024-07-02 12:15:28 +0530

    Add files in folder dotnet Practicle 2 Folder (02-07-2024_06-42-07)

---
+++ dotnet Practicle 2 Folder (02-07-2024_06-42-07)/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using dotnet_project_2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace dotnet_project_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ViewResult Index()
+        {
+            return View("MyView");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        
+    }
+}
---
+++ dotnet Practicle 2 Folder (02-07-2024_06-42-07)/Views/Home/MyView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnet_project_2.Views.Home
+{
+    public class MyViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
