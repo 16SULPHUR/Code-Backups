6730c7ebf554ee888d388370336c981c5180597e
Ankit Patil <123143098+16SULPHUR@users.noreply.github.com>
2024-07-09 12:26:35 +0530

    Add files in folder ASP .NET PRACTICLE 4  (09-07-2024_06-53-03)

---
+++ ASP .NET PRACTICLE 4  (09-07-2024_06-53-03)/practicle4/Views/Home/Welcome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace practicle4.Views.Home
+{
+    public class WelcomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
