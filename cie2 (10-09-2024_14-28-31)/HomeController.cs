2b7683d4f820ec51652cda6629a6535aedbc668c
Ankit Patil <123143098+16SULPHUR@users.noreply.github.com>
2024-09-10 14:30:09 +0530

    Add files in folder cie2 (10-09-2024_14-28-31)

---
+++ cie2 (10-09-2024_14-28-31)/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace cie2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Action to display the main page and show current cookie value
+        public IActionResult Index()
+        {
+            // Retrieve the cookie value if it exists
+            string cookieValue = Request.Cookies["MyCookie"];
+
+            // Pass the cookie value to the view
+            ViewData["CookieValue"] = cookieValue;
+
+            return View();
+        }
+
+        // Action to set a cookie with a custom value
+        [HttpPost]
+        public IActionResult SetCustomCookie(string cookieValue)
+        {
+            if (!string.IsNullOrEmpty(cookieValue))
+            {
+                Response.Cookies.Append("MyCookie", cookieValue, new CookieOptions
+                {
+                    Expires = DateTimeOffset.Now.AddMinutes(5) // Cookie expires in 5 minutes
+                });
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // Action to delete the cookie
+        public IActionResult DeleteCookie()
+        {
+            // Delete the cookie
+            Response.Cookies.Delete("MyCookie");
+
+            return RedirectToAction("Index");
+        }
+    }
+}
